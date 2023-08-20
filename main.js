3c5b66d87cedf48c59e1c3de03dabbe58944cc9e
Zeet33177 <115207805+Zeet33177@users.noreply.github.com>
2023-08-20 12:30:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 let count = 0
 let STR=document.getElementById("str").textContent;
+const cn=document.getElementById("counter");
 
 function increment() {
   count = count + 1
   document.getElementById("counter").innerText = count
+  if(count>0)
+cn.style.color="green"
+if(count==0)
+cn.style.color="white"
+
 }
+
+
+
 function decrement(){
   count = count - 1
   document.getElementById("counter").innerText = count
+  if(count<0)
+cn.style.color="#BF2838"
+  if(count==0)
+cn.style.color="white"
+
   
 }
 
@@ -15,4 +29,6 @@ function save() {
   document.getElementById("str").textContent+=count+", ";
   count = 0
   document.getElementById("counter").innerText = count
+    cn.style.color = "white"
+  
 }
